3546a8d65e301b53f197123b9f866623aa36c922
Code Nation 20 <codenation@CN-20.lan>
2019-03-05 10:53:28 +0000

    Can change how many posts are displayed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -7,13 +7,22 @@ class Feed extends React.Component{
     
 
     render(){
+
+        let maxPosts = 5
+
+        let postArr = []
+        for (let i = 0; i < maxPosts; i++){
+            postArr.push(
+                <Post
+                />
+            )
+        }
+
         return(
             <div className="Feed">
                 <h1>Feed</h1>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                {postArr}
+                
             </div>
         )
     }
