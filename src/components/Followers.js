2f0193a0467d16767baf702eeec9d9aba07160bb
Sharmarke Yusuf Mussa <touche@Sharmarkes-MBP.lan>
2019-03-06 10:13:00 +0000

    first

---
+++ src/components/Followers.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import './Followers.css';
+
+class Followers extends React.Component{
+
+    render(){
+        return(
+            <div className="Followers">
+                <h1>Followers 23</h1>
+                
+
+            </div>
+        )
+    }
+
+
+}
+
+export default Followers
--- src/components/RightNav.js
+++ src/components/RightNav.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import logoyellowbg from './logoyellowbg.svg';
+// import logoyellowbg from './logoyellowbg.svg';
 import "./RightNav.css";
 
+import Followers from "./Followers"
+import Following from "./Following"
 
 class RightNav extends React.Component {
 
     render() {
         return (
             <div>
-                <div>
+                {/* <div>
                     <img src={logoyellowbg} className="App-logo" alt="logo" />
                     <h1>duck.io</h1>
-                </div>
+                </div> */}
                 <h1 className="RightNav">RightNav</h1>
+                <div className="Followers"><Followers /></div>
+                <div className="Following"><Following /></div>
             </div>
+            
         )
     }
 
