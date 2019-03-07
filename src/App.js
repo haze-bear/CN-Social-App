0c61a65cdbf702c835895c391a8ef502037feb3b
Ashley Bennett <codenation@CN-20.lan>
2019-03-07 11:02:08 +0000

    Fixed feed to update as new post on top

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
         />
         
         this.setState(prevState => ({
-          post: [...prevState.post, posts], term:"",
+          post: [posts, ...prevState.post], term:"",
         }))
   }
 
