b2812d7fda1ce553b84389cdf24f7b314af88388
Ashley Bennett <codenation@CN-20.local>
2019-03-08 17:34:05 +0000

    attempt at rss

--- src/components/Homepage/Trending.js
+++ src/components/Homepage/Trending.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import * as rssParser from 'react-native-rss-parser';
+ 
+
+
 
 class Trending extends React.Component {
 
+    constructor() {
+        super()
+        this.state = {
+            feed:[],
+        }
+    }
+    
+    x = () => {
+        return fetch('http://www.nasa.gov/rss/dyn/breaking_news.rss')
+    .then((response) => response.text())
+    .then((responseData) => rssParser.parse(responseData))
+    .then((rss) => {
+     console.log(rss.title);
+     console.log(rss.items.length);
+     this.setState({feed: rss.title})
+    })
+    };
+
+    componentDidMount() {
+        this.x()
+    }
+
     render() {
+
         return (
             <div className="Trending">
                 <div><h1><font color="#27B1BE">Trending</font></h1></div>
+                <h1>{this.state.feed}</h1>
             </div>
         )
     }
