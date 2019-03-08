ee3a69e922a4e5bdf0d2f3ec089e3a09be4b2307
Ashley Bennett <codenation@CN-20.lan>
2019-03-08 14:02:45 +0000

    added basic interaction on each post action

--- src/components/Homepage/Post.js
+++ src/components/Homepage/Post.js
@@ -11,6 +11,8 @@ class Post extends React.Component{
             hoverSave: false,
             hoverComment: false,
             clickedLike: false,
+            clickedSave: false,
+            clickedComment: false,
         }
     }
 
@@ -39,8 +41,7 @@ class Post extends React.Component{
 
     }
 
-    clicked = (state) => {
-        console.log("hello")
+    clickedLike = (state) => {
         console.log(state)
         if(state){
             this.setState({clickedLike: false})
@@ -49,8 +50,79 @@ class Post extends React.Component{
         }
     }
 
+    clickedSave = (state) => {
+        console.log(state)
+        if(state){
+            this.setState({clickedSave: false})
+        } else {
+            this.setState({clickedSave: true})
+        }
+    }
+
+    clickedComment = (state) => {
+        console.log(state)
+        if(state){
+            this.setState({clickedComment: false})
+        } else {
+            this.setState({clickedComment: true})
+        }
+    }
+
+    checkClicked = (clicked) => {
+        // console.log("hello")
+        if (!clicked){
+            if (this.state.hoverLike){
+                return "fas fa-heart"
+            } else {
+            return  "far fa-heart"
+            }
+        } else {
+            return "far fa-heart"
+        }
+    }
+
 
     render(){
+
+        const clickedLike = () => {
+            console.log("hello")
+            if (!this.state.clickedLike){
+                if (this.state.hoverLike){
+                    return "fas fa-heart"
+                } else {
+                return  "far fa-heart"
+                }
+            } else {
+                return "fas fa-heart"
+            }
+        }
+
+        const clickedSave = () => {
+            console.log("hello")
+            if (!this.state.clickedSave){
+                if (this.state.hoverSave){
+                    return "fas fa-bookmark"
+                } else {
+                return  "far fa-bookmark"
+                }
+            } else {
+                return "fas fa-bookmark"
+            }
+        }
+
+        const clickedComment = () => {
+            console.log("hello")
+            if (!this.state.clickedComment){
+                if (this.state.hoverComment){
+                    return "fas fa-comment-alt"
+                } else {
+                return  "far fa-comment-alt"
+                }
+            } else {
+                return "fas fa-comment-alt"
+            }
+        }
+
         return(
             <div className="postCont">
                 <div className="postHeader">
@@ -63,20 +135,23 @@ class Post extends React.Component{
                         <div className="postIconLeft"><i
                         onMouseEnter={() => this.setButtonHoveredLike(true)} 
                         onMouseLeave={() => this.setButtonHoveredLike(false)}
-                        onMouseUp={() => this.clicked(this.state.clickedLike)}
-                        class={this.state.hoverLike ? "fas fa-heart" : "far fa-heart"}
-                        
-                        
+                        onMouseUp={() => this.clickedLike(this.state.clickedLike)}
+                        class={clickedLike()}
                         ></i></div>
                         <div className="postIconCenter"><i 
                         onMouseEnter={() => this.setButtonHoveredSave(true)} 
                         onMouseLeave={() => this.setButtonHoveredSave(false)}
-                        class={this.state.hoverSave ? "fas fa-bookmark" : "far fa-bookmark"}>
+                        // class={this.state.hoverSave ? "fas fa-bookmark" : "far fa-bookmark"}
+                        onMouseUp={() => this.clickedSave(this.state.clickedSave)}
+                        class={clickedSave()}
+                        >
                         </i></div>
                         <div className="postIconRight"><i
                         onMouseEnter={() => this.setButtonHoveredComment(true)} 
                         onMouseLeave={() => this.setButtonHoveredComment(false)}
-                        class={this.state.hoverComment ? "fas fa-comment-alt" : "far fa-comment-alt"}
+                        // class={this.state.hoverComment ? "fas fa-comment-alt" : "far fa-comment-alt"}
+                        onMouseUp={() => this.clickedComment(this.state.clickedComment)}
+                        class={clickedComment()}
                          ></i></div>
                     </div>
                 </div>
